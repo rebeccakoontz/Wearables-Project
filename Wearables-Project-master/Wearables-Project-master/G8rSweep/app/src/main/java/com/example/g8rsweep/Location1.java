6833a27914f774eb311a8c8ca65efa99fb99a6b4
rebeccakoontz <46692638+rebeccakoontz@users.noreply.github.com>
2019-12-11 02:13:16 -0500

    implemented finished layouts, descriptions, and attempted buttons
    attempted buttons on location 1

--- Wearables-Project-master/Wearables-Project-master/G8rSweep/app/src/main/java/com/example/g8rsweep/Location1.java
+++ Wearables-Project-master/Wearables-Project-master/G8rSweep/app/src/main/java/com/example/g8rsweep/Location1.java
@@ -2,8 +2,13 @@ package com.example.g8rsweep;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import static android.net.sip.SipErrorCode.TIME_OUT;
 
 public class Location1 extends AppCompatActivity {
 
@@ -14,5 +19,16 @@ public class Location1 extends AppCompatActivity {
 
         MediaPlayer sandcastle = MediaPlayer.create(Location1.this, R.raw.sandcastle);
         sandcastle.start();
+    }
+
+    public void onClickMainMenu(View view){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Location1.this, EntryActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
     }
 }
